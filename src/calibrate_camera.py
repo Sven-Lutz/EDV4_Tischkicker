332da179f29bde79481f92c3ff892324ba986280
Agabekov <marcel.agabekov@porsche.de>
2026-04-10 16:05:12 +0200

    src/calibrate_camera.py

--- src/calibrate_camera.py
+++ src/calibrate_camera.py
@@ -1,32 +1,37 @@
-# calibrate_camera.py
-import cv2
 import numpy as np
+import cv2 as cv
 import glob
 
-CHECKERBOARD = (9, 6)
-criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+# termination criteria
+criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
-objp = np.zeros((CHECKERBOARD[0]*CHECKERBOARD[1], 3), np.float32)
-objp[:, :2] = np.mgrid[0:CHECKERBOARD[0], 0:CHECKERBOARD[1]].T.reshape(-1, 2)
+# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+objp = np.zeros((6 * 7, 3), np.float32)
+objp[:, :2] = np.mgrid[0:7, 0:6].T.reshape(-1, 2)
 
-objpoints = []
-imgpoints = []
+# Arrays to store object points and image points from all the images.
+objpoints = []  # 3d point in real world space
+imgpoints = []  # 2d points in image plane.
 
-images = glob.glob("calibration/*.jpg")
+images = glob.glob('calibration/*.jpg')
 
-for img_path in images:
-    img = cv2.imread(img_path)
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+for fname in images:
+    img = cv.imread(fname)
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
-    ret, corners = cv2.findChessboardCorners(gray, CHECKERBOARD, None)
-    if ret:
-        corners = cv2.cornerSubPix(gray, corners, (11,11), (-1,-1), criteria)
+    # Find the chess board corners
+    ret, corners = cv.findChessboardCorners(gray, (7, 6), None)
+
+    # If found, add object points, image points (after refining them)
+    if ret == True:
         objpoints.append(objp)
-        imgpoints.append(corners)
 
-ret, camera_matrix, dist_coeffs, _, _ = cv2.calibrateCamera(
-    objpoints, imgpoints, gray.shape[::-1], None, None
-)
+        corners2 = cv.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
+        imgpoints.append(corners2)
 
-np.save("camera_matrix.npy", camera_matrix)
-np.save("dist_coeffs.npy", dist_coeffs)
+        # Draw and display the corners
+        cv.drawChessboardCorners(img, (7, 6), corners2, ret)
+        cv.imshow('img', img)
+        cv.waitKey(500)
+
+cv.destroyAllWindows()
